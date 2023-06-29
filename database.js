fb29efb5a1ba4d5df6e17ed8255dcae3482065b1
Casey <leptone55@gmail.com>
2023-06-29 15:44:40 -0700

    insecure

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require('mongodb')
 
 // const url = process.env.DB_URL
-const url = process.env.MONGODB_URI
+// const url = process.env.MONGODB_URI
+const url =
+  'mongodb+srv://dopevisualz:dopevisualz@cluster0.ksvw7wm.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(url)
 
 const dbName = 'dopevisualz'
